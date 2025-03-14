dcb3ff6d928572a96aef72bf1e4eee1691706992
nn-hoshi <nn.hoshi4@gmail.com>
2025-03-14 15:07:31 +0300

    1st commit

--- obj/Debug/net8.0/BackLR3.AssemblyInfo.cs
+++ obj/Debug/net8.0/BackLR3.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BackLR3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ea9d99b679ac320bd71ca06e3a1377618f536ec6")]
 [assembly: System.Reflection.AssemblyProductAttribute("BackLR3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BackLR3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
